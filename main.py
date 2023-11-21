aada91c69b6924bcea490c267658fd26d81a9b76
Elka_ <example@example.com>
2023-11-21 18:02:38 +0300

    task

--- main.py
+++ main.py
@@ -9,23 +9,6 @@ from random import randint
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_Form(object):
-    def setupUi(self, Form):
-        Form.setObjectName("Form")
-        Form.resize(728, 568)
-        self.pushButton = QtWidgets.QPushButton(Form)
-        self.pushButton.setGeometry(QtCore.QRect(250, 510, 241, 41))
-        self.pushButton.setObjectName("pushButton")
-
-        self.retranslateUi(Form)
-        QtCore.QMetaObject.connectSlotsByName(Form)
-
-    def retranslateUi(self, Form):
-        _translate = QtCore.QCoreApplication.translate
-        Form.setWindowTitle(_translate("Form", "Form"))
-        self.pushButton.setText(_translate("Form", "НАЖМИ МЕНЯ"))
-
-
 class MyPillow(QWidget, circles.Ui_Form):
     def __init__(self):
         super().__init__()
